9516d988f78c66dfa817c9e7c9fa28f88a161629
leekuhyunk <tvvv1@naver.com>
2021-04-19 15:57:28 +0900

    커밋

--- src/main/java/org/edu/dao/MemberDAOImpl.java
+++ src/main/java/org/edu/dao/MemberDAOImpl.java
@@ -7,8 +7,10 @@ import java.util.Map;
 import javax.inject.Inject;
 
 import org.apache.ibatis.session.SqlSession;
+import org.edu.vo.ChartVO;
 import org.edu.vo.MemberVO;
 import org.edu.vo.PageVO;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -18,17 +20,17 @@ public class MemberDAOImpl implements IF_MemberDAO {
 	
 	@Override
 	public List<MemberVO> selectMember(PageVO pageVO) throws Exception {
-		// mapper.xml에 접근하는 방법(아래)	
-		//오브젝트의 메서드를 실행할때, 
-		//.입력힌트가 나오지 않으면 Ctrl+Space키를 눌러서 메서드 나오게 합니다.
-		//매개변수를 1개로 만들어서 값을 2개 집어넣는 방식=해시데이터를 만들어서 구현.
+		// mapper.xml에 접근하는 방법(아래)
+		//오브젝트의 메서드를 실행할때,
+		//.입력힌트가 나오지 않으면, Ctrl+Space키를 눌러서 메서드 나오게 합니다.
+		//매개변수를 1개로 만들어서 값을 2개 집어넣는 방식-해시데이터 타입을 만들어서 구현.
 		/*
 		 * Map<String, Object> paramMap = new HashMap<String, Object>();
 		 * paramMap.put("search_type", search_type); paramMap.put("search_keyword",
 		 * search_keyword);
 		 */
-		return sqlSession.selectList("memberMapper.selectMember",pageVO) ;
-		//sqlSession템플릿의selectList메서드를 실행하면, memberMapper.selectMember의 쿼리를 실행하게됨.
+		return sqlSession.selectList("memberMapper.selectMember",pageVO);
+		//sqlSession템플릿의 selectList메서드를 실행하면, memberMapper.selectMember의 쿼리를 실행하게됨.
 	}
 
 	@Override
@@ -39,13 +41,13 @@ public class MemberDAOImpl implements IF_MemberDAO {
 
 	@Override
 	public void deleteMember(String user_id) throws Exception {
-		// mapper.xml에 접근하는  방법(아래)
-		sqlSession.delete("memberMapper.deleteMember", user_id);
+		// mapper.xml에 접근하는 방법(아래)
+		sqlSession.delete("memberMapper.deleteMember", user_id);	
 	}
 
 	@Override
 	public MemberVO readMember(String user_id) throws Exception {
-		// mapper.xml에 접근하는 방법(아래) .selectone() sql세션템플릿안메서드 입니다.
+		// mapper.xml에 접근하는 방법(아래) .selectOne() sql세션템플릿안 메서드입니다.
 		return sqlSession.selectOne("memberMapper.readMember", user_id);
 	}
 
@@ -60,5 +62,29 @@ public class MemberDAOImpl implements IF_MemberDAO {
 		// mapper.xml에 접근하는 방법(아래)
 		return sqlSession.selectOne("memberMapper.countMember", pageVO);
 	}
-	
+
+	@Override
+	public ChartVO getData() {
+		// mapper.xml에 접근하는 방법(아래)
+		return sqlSession.selectOne("memberMapper.getData");
+	}
+
+	@Override
+	public void insertData(ChartVO chartVO) {
+		// mapper.xml에 접근하는 방법(아래)
+		sqlSession.insert("memberMapper.insertData", chartVO);
+	}
+
+	@Override
+	public void updateData(ChartVO chartVO) {
+		// mapper.xml에 접근하는 방법(아래)
+		sqlSession.update("memberMapper.updateData", chartVO);
+	}
+
+	@Override
+	public void delData() {
+		// mapper.xml에 접근하는 방법(아래)
+		sqlSession.delete("memberMapper.delData");
+	}
+
 }
